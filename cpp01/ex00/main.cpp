1e98c17ec76f43cd9796fd750dafbacc429e7c85
NilsLeVrai <nilslucas@wanadoo.fr>
2025-01-06 18:11:21 +0100

    cpp1 ex00 done

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 15:36:48 by niabraha          #+#    #+#             */
-/*   Updated: 2024/12/28 17:55:28 by niabraha         ###   ########.fr       */
+/*   Updated: 2025/01/06 14:58:13 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ int main(void) {
 
 	Zombie *zombie1 = newZombie("Natanael");
 	zombie1->announce();
-	delete zombie1;
 	randomChump("Vulvette");
+	delete zombie1;
 }
--- cpp01/ex00/randomChump.cpp
+++ cpp01/ex00/randomChump.cpp
@@ -6,13 +6,13 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 17:13:15 by niabraha          #+#    #+#             */
-/*   Updated: 2024/12/28 17:21:54 by niabraha         ###   ########.fr       */
+/*   Updated: 2025/01/06 14:57:39 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-void randomChump( std::string name){
+void randomChump( std::string name ){
 	
 	Zombie zombie = Zombie(name);
 	zombie.announce();
