b1465e223c4c75880ca1096e88acc3a09ce277ea
NilsLeVrai <nilslucas@wanadoo.fr>
2025-01-07 17:59:33 +0100

    cpp1 ex04 euh

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,16 +6,33 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 17:54:33 by niabraha          #+#    #+#             */
-/*   Updated: 2025/01/06 18:21:31 by niabraha         ###   ########.fr       */
+/*   Updated: 2025/01/07 17:58:00 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
+void replace(std::string filename, std::string s1, std::string s2)
+{
+    std::ofstream outfile;
+    std::string::size_type pos;
+
+    outfile.open((filename + ".replace").c_str());
+    for (std::string::size_type i = 0; i < filename.length(); i++) {
+        pos = filename.find(s1, i);
+        if (pos != std::string::npos) {
+            outfile << filename.substr(i, pos - i) << s2;
+            i = pos + s1.length() - 1;
+        } else {
+            outfile << filename[i];
+        }
+    }
+    outfile.close();
+}
+
 bool validateArguments(int argc, char **argv)
 {
-	
 	std::ifstream file(argv[1]);
 	if (argc != 4)
 	{
@@ -45,4 +62,6 @@ int main(int argc, char **argv)
 		return (1);
 	}
 	std::cout << "File opened successfully." << std::endl;
+	replace(argv[1], argv[2], argv[3]);
+	return (0);
 }
