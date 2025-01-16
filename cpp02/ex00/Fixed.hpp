196515c95e7a0bd9a1a9c76caa0ec199007784cc
NilsLeVrai <nilslucas@wanadoo.fr>
2025-01-16 18:06:15 +0100

    cpp2 ex00 done

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/08 17:22:59 by niabraha          #+#    #+#             */
-/*   Updated: 2025/01/16 17:45:16 by niabraha         ###   ########.fr       */
+/*   Created: 2025/01/16 18:02:18 by niabraha          #+#    #+#             */
+/*   Updated: 2025/01/16 18:05:52 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 class Fixed {
 
 	private:
-		int					_value;
-		static const int	_fracBits;
-	
+		int _value;
+		static const int _fracBits;
+
 	public:
 		Fixed(void);
 		Fixed(Fixed const & src);
