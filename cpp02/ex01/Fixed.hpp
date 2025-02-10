431c9f9148eac5d6b8fa758d5635d0e9af322d81
NilsLeVrai <nilslucas@wanadoo.fr>
2025-02-10 15:47:39 +0100

    10/02 cpp2 ex00 done + notes

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 17:46:54 by niabraha          #+#    #+#             */
+/*   Updated: 2025/02/10 13:04:40 by niabraha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+
+	private:
+		int _value;
+		static const int _fractional_bits = 8;
+		
+	public:
+		Fixed();
+		Fixed(const Fixed &src);
+		Fixed(const int value);
+		Fixed(const float value);
+		~Fixed();
+		Fixed &operator=(const Fixed &src);
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
+};
