dd3f3c131885566f2bee708eec3475152009e664
NilsLeVrai <nilslucas@wanadoo.fr>
2025-02-11 16:42:05 +0100

    11/02 add modif try catch

--- cpp02/ex02/example.cpp
+++ cpp02/ex02/example.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   example.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 17:46:17 by niabraha          #+#    #+#             */
-/*   Updated: 2025/02/10 16:52:38 by niabraha         ###   ########.fr       */
+/*   Updated: 2025/02/11 16:41:49 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 
 int main() {
 	int a = 42;
-	int b = 03;
+	int b = 0;
 	float c = 42.42f;
 	float d = 0.0f;
 
+	//std::cout << a / b << std::endl;
+	//std::cout << c / d << std::endl;
 	try {
 		if (b == 0) {
 			throw std::runtime_error("Division by zero (int)");
