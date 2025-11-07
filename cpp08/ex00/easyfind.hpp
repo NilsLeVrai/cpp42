09542978b941a55a048635ac5ad15a7d0a8ba120
NilsLeVrai <nilslucas@wanadoo.fr>
2025-11-07 17:30:55 +0100

    cpp8 ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,13 +1,16 @@
 #pragma once
 
 #include <algorithm>
+#include <vector>
+#include <iostream>
+#include <list>
 
 template <typename T>
-typename T::const_iterator (T& type, int value) {
-    typename T::iterator it = type.begin();
-    for (; it != type.end(); it++) {
-        if (value == *it)
-            break;
-    }
-    return (it);
+typename T::iterator easyfind(T& type, int value) {
+    typename T::iterator it = std::find(type.begin(), type.end(), value);
+    if (it == type.end())
+        throw std::out_of_range("Element not found in this container");
+    return it;
 }
+
+//const ^^
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,7 +1,38 @@
-#include <iostream>
+#include "easyfind.hpp"
 
 int main() {
     
+    std::vector<int> vec1;
+
+    vec1.push_back(1);
+    vec1.push_back(10);
+    vec1.push_back(-5);
+
+    std::list<char> lst1;
+
+    lst1.push_back('a');
+    lst1.push_back('b');
+    lst1.push_back('c');
+
+    try {
+        std::vector<int>::iterator it1 = easyfind(vec1, 10);
+        std::cout << "Value found: " << *it1 << std::endl;
+
+        std::vector<int>::iterator it2 = easyfind(vec1, 0);
+        std::cout << "Value found: " << *it2 << std::endl;
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    try {
+        std::list<char>::iterator it3 = easyfind(lst1, 'b');
+        std::cout << "Value found: " << *it3 << std::endl;
+
+        std::list<char>::iterator it4 = easyfind(lst1, 'j');
+        std::cout << "Value found: " << *it4 << std::endl;
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
     
     return 0;
 }
